a3814ffb8675338df5c0d447f1eaab22a3636eb5
interk0t <interkotk@mail.ru>
2024-11-22 23:08:38 +0300

    upd3

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    server: {
+        port: process.env.PORT || 4173, // Используем порт из переменной окружения или 4173 по умолчанию
+    },
+    plugins: [sveltekit()],
 });
